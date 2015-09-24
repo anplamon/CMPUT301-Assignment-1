baf973d94b50413b5a0f1114b280e281a12f627d
anplamon <anplamon@ualberta.ca>
2015-09-24 16:07:25 -0600

    Changed the name of the default activity from buzzer to appMenu. Made the main menu buttons open up a seperate activity.

--- app/src/main/java/com/example/aaronplamondon/anplamon_reflex/appMenu.java
+++ app/src/main/java/com/example/aaronplamondon/anplamon_reflex/appMenu.java
@@ -1,16 +1,43 @@
 package com.example.aaronplamondon.anplamon_reflex;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class buzzer extends AppCompatActivity {
+public class appMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buzzer);
+
+
+        // Code from stack overflow from user Nic007
+        Button statisticsButton= (Button) findViewById(R.id.statisticsButton);
+        statisticsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(appMenu.this, statistics.class));
+            }
+        });
+
+        Button multiplayerButton= (Button) findViewById(R.id.multiplayerButton);
+        multiplayerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(appMenu.this, multiplayer.class));
+            }
+        });
+
+        Button reactionButton= (Button) findViewById(R.id.reactionButton);
+        reactionButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(appMenu.this, reflexGame.class));
+            }
+        });
+
     }
 
     @Override
