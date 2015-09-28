318a2462fab28ad4a258742faecbd69ccf4f8876
anplamon <anplamon@ualberta.ca>
2015-09-28 16:21:00 -0600

    Added a slider to determine the number of players in the gameshow mode

--- app/src/main/java/com/example/aaronplamondon/anplamon_reflex/multiplayer.java
+++ app/src/main/java/com/example/aaronplamondon/anplamon_reflex/multiplayer.java
@@ -4,13 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 public class multiplayer extends AppCompatActivity {
+    private SeekBar playerBar;
+    private TextView textView;
+    private Integer numberOfPlayers;
+    private Integer seekBarStep = 33;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplayer);
+        textView = (TextView) findViewById(R.id.textView);
+        playerBar = (SeekBar) findViewById(R.id.playerBar);
+        playerBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            Integer progressValue = 0;
+
+            @Override
+            //on progress method from http://stackoverflow.com/questions/16272053/how-to-increment-seek-bar-value
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressValue = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //do nothing
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                progressValue = ((int) Math.round(progressValue / (double) seekBarStep)) * seekBarStep;
+                numberOfPlayers = progressValue + 2;
+                seekBar.setProgress(progressValue);
+            }
+        });
     }
 
     @Override
